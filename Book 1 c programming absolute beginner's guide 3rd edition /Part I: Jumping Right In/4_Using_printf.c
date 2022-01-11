829bbb931dd5f715d6e7b8da326e1bd39a3fc9ed
tashinva <vikashsinghshekhawat088941@gmail.com>
2022-01-11 14:28:15 +0530

    Solved

---
+++ Book 1 c programming absolute beginner's guide 3rd edition /Part I: Jumping Right In/4_Using_printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("\n\n\n\n");
+
+    printf("Column A\tColumn B\tColumn C");
+    printf("\nMy Computer\'s Beep Sounds Like This: \a!\n");
+    printf("\"Letz\bs fix that typo and then show the backslash ");
+    printf("character \\\" she said\n");
+
+    printf("\n\n\n\n");
+
+    return 0;
+}
