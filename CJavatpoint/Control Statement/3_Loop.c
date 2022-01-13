b67891333c51b98b32ac051f4776bd8ed5af2db8
tashinva <vikashsinghshekhawat088941@gmail.com>
2022-01-13 10:37:44 +0530

    13 Jan 2022

--- CJavatpoint/Control Statement/3_Loop.c
+++ CJavatpoint/Control Statement/3_Loop.c
@@ -2,8 +2,45 @@
 int dowhileloop();
 int whileloop();
 int forloop();
-int main () {
-
-
+int main()
+{
+    dowhileloop();
+    whileloop();
+    forloop();
     return 0;
+}
+
+int dowhileloop()
+{
+    int a = 0;
+    do
+    {
+        printf("%d\n", a);
+        a++;
+    } while (a < 10);
+
+    printf("---------------------------------\n");
+    return 0;
+}
+
+int whileloop()
+{
+    int a = 0;
+    while (a < 10)
+    {
+        printf("%d\n", a);
+        a++;
+    }
+
+    printf("---------------------------------\n");
+    return 0;
+}
+
+int forloop()
+{
+    int a = 0;
+    for (a = 0; a < 10; a++)
+    {
+        printf("%d\n", a);
+    }
 }
---
+++ CJavatpoint/Control Statement/4_DoWhileLoop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int Example1();
+int Example2();
+int Example3();
+int main()
+{
+
+
+
+    return 0;
+}
+
